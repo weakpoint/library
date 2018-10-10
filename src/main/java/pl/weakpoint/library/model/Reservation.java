1310daf8943d4c07fdfcc5b50c3074dabe0a4c25
pluciennikowski <pluciennikowski@tt.com.pl>
2018-10-10 13:24:12 +0200

    fixed join in reservations

--- src/main/java/pl/weakpoint/library/model/Reservation.java
+++ src/main/java/pl/weakpoint/library/model/Reservation.java
@@ -3,10 +3,15 @@ package pl.weakpoint.library.model;
 import java.io.Serializable;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Version;
 import javax.validation.constraints.NotNull;
@@ -27,7 +32,8 @@ public class Reservation implements Serializable{
 	@NotNull
 	private User owner;
 	
-    @OneToMany
+	@ManyToMany
+	@JoinTable(name = "Reservation_Book", joinColumns = { @JoinColumn(name = "id") }, inverseJoinColumns = { @JoinColumn(name = "isbn") })
     @NotNull
 	private List<Book> books;
 	
